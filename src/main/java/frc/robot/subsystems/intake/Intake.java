ae9591267ee352f5e99d37496609a483f8ae038d
RyanVail <111651152+RyanVail@users.noreply.github.com>
2026-03-09 20:48:18 -0400

    Revert "Improve intake"
    This reverts commit f37509ba754ad2a62dc062aaaa430e8f1ed4c79e.

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -14,41 +14,26 @@ import frc.robot.DoubleSupplier;
 import frc.robot.PIDSupplier;
 
 public class Intake extends SubsystemBase {
-    public enum State {
-        // Held high to keep over bump.
-        Idle,
-
-        // Held down to intake fuel.
-        Intaking,
-
-        // Oscillating to help feed fuel into the shooter.
-        Oscillating,
-
-        // Slowly crunch inward to feed fuel into the shooter.
-        Crunching,
-    }
-
     IntakeIO io;
-    State state = State.Idle;
-    double rotSetpoint;
-
-    double shootStart;
 
     PIDSupplier rotPID = new PIDSupplier(LPREFIX + "rotPID", new PIDConstants(0.05));
     ArmFeedforward rotFF = new ArmFeedforward(0.00002, 0.000185, 0.00002);
     TrapezoidProfile rotProfile = new TrapezoidProfile(new TrapezoidProfile.Constraints(
-            20.0,
-            5.0));
+        20.0,
+        5.0
+    ));
 
-    DoubleSupplier ossilateScale = new DoubleSupplier(LPREFIX + "ossilate", 20.0);
-    DoubleSupplier crunchSpeed = new DoubleSupplier(LPREFIX + "crunchSpeed", 4.0);
-
-    DoubleSupplier upPos = new DoubleSupplier(LPREFIX + "upPos", 30.0);
-    DoubleSupplier idlePos = new DoubleSupplier(LPREFIX + "rotIdle", 60.0);
-    DoubleSupplier intakePos = new DoubleSupplier(LPREFIX + "intakePos", 80.0);
+    DoubleSupplier rotUpPos = new DoubleSupplier(LPREFIX + "rotUpPos", 0.0);
+    DoubleSupplier rotDownPos = new DoubleSupplier(LPREFIX + "rotDownPos", 80.0);
+    DoubleSupplier ossilateScale = new DoubleSupplier(LPREFIX + "Ossilate", 20.0);
 
     DoubleSupplier voltage = new DoubleSupplier(LPREFIX + "voltage", 10.0);
     DoubleSupplier shootingVoltage = new DoubleSupplier(LPREFIX + "shootingVoltage", 2.0);
+
+    double rotSetpoint;
+    boolean spinning = false;
+    boolean shooting = false;
+    boolean down = false;
 
     private static final String LPREFIX = "/Subsystems/Intake/";
 
@@ -64,27 +49,41 @@ public class Intake extends SubsystemBase {
         double rotPosition = getRotPosition();
         double rotVelocity = getRotVelocity();
 
-        rotSetpoint = nextRotSetpoint();
         TrapezoidProfile.State state = rotProfile.calculate(
-                Constants.LOOP_TIME,
-                new TrapezoidProfile.State(rotPosition, rotVelocity),
-                new TrapezoidProfile.State(rotSetpoint, 0.0));
+            Constants.LOOP_TIME,
+            new TrapezoidProfile.State(rotPosition, rotVelocity),
+            new TrapezoidProfile.State(rotSetpoint, 0.0)
+        );
+
+        if (shooting) {
+            double x = (Math.sin(Timer.getFPGATimestamp() * 9.0) + 1.0) * 0.5;
+            rotSetpoint = rotDownPos.get() - (x * ossilateScale.get());
+        } else if (down) {
+            rotSetpoint = rotDownPos.get();
+        } else {
+            rotSetpoint = rotUpPos.get();
+        }
 
         rotPID.get().setSetpoint(rotSetpoint);
-
-        double rotOutput = rotPID.get().calculate(state.position);
+        double rotOutput = rotPID.get().calculate(rotPosition);
 
         rotOutput += rotFF.calculate(
-                Units.degreesToRadians(rotPosition - 90.0),
-                Units.degreesToRadians(state.velocity));
+            Units.degreesToRadians(rotPosition - 90.0),
+            state.velocity
+        );
 
         io.setRotVoltage(rotOutput);
 
         Logger.recordOutput(LPREFIX + "RotSetpoint", rotSetpoint);
         Logger.recordOutput(LPREFIX + "RotPosition", rotPosition);
         Logger.recordOutput(LPREFIX + "RotOutput", rotOutput);
-
-        double targetVoltage = nextTargetVoltage();
+ 
+        double targetVoltage = 0.0;
+        if (spinning) {
+            targetVoltage = voltage.get();
+        } else if (shooting) {
+            targetVoltage = shootingVoltage.get();
+        }
 
         Logger.recordOutput(LPREFIX + "TargetVoltage", targetVoltage);
         setVoltage(targetVoltage);
@@ -92,41 +91,36 @@ public class Intake extends SubsystemBase {
         io.simulationPeriodic();
     }
 
-    private double nextRotSetpoint() {
-        double time = Timer.getFPGATimestamp() - shootStart;
-        return switch (state) {
-            case Intaking -> intakePos.get();
-            case Oscillating ->
-                intakePos.get() - ((Math.sin(time * 9.0) + 1.0) * 0.5 * ossilateScale.get());
-            case Crunching -> Math.max(intakePos.get() - time * crunchSpeed.get(), upPos.get());
-            default -> idlePos.get();
-        };
-    }
-
-    private double nextTargetVoltage() {
-        return switch (state) {
-            case Intaking -> voltage.get();
-            case Oscillating -> shootingVoltage.get();
-            case Crunching -> shootingVoltage.get();
-            default -> 0.0;
-        };
-    }
-
     public void start() {
-        state = State.Intaking;
+        spinning = true;
     }
 
     public void stop() {
-        state = State.Idle;
+        spinning = false;
+    }
+
+    public void moveDown() {
+        down = true;
+    }
+
+    public void moveUp() {
+        down = false;
     }
 
     public void startShooting() {
-        shootStart = Timer.getFPGATimestamp();
-        state = State.Oscillating;
+        shooting = true;
     }
 
     public void stopShooting() {
-        state = State.Idle;
+        shooting = false;
+    }
+
+    public void toggleDown() {
+        if (down) {
+            moveUp();
+        } else {
+            moveDown();
+        }
     }
 
     public void setVoltage(double voltage) {
